61b25f5af6660692501ee254e31d5e5c15108ad0
buwai <buwai.bw@qq.com>
2014-10-30 23:40:55 +0800

    Signed-off-by: buwai <buwai.bw@qq.com>

---
+++ samples/Application.mk
@@ -0,0 +1,3 @@
+APP_STL := gnustl_static
+NDK_TOOLCHAIN_VERSION=4.9
+APP_CPPFLAGS := -frtti -fexceptions -std=gnu++11
---
+++ samples/copyFormAssets.cpp
@@ -0,0 +1,52 @@
+﻿#include <utils/SmartPoint.h>
+#include <JNIJava/JavaBase.h>
+#include <JNIJava/AndroidContent.h>
+#include <JNIJava/JavaIO.h>
+
+/**
+ * 将assets中的文件拷贝出来。
+ * @param[in] jfileName assets中的文件名。
+ * @param[in] joutPath 输出路径。
+ */
+void copyFormAssets(jstring jfileName, jstring joutPath) {
+	// 上下文。
+	JContext context;
+	context.InitJObject(obj);
+
+	// assets目录中的文件名。
+	JString fileName;
+	fileName.InitJObject(jfileName);
+
+	// 输出目录。
+	JString outPath;
+	outPath.InitJObject(joutPath);
+
+	JFile fileOutPath;
+	fileOutPath.New(outPath);
+
+	// AssetManager。
+	lsp<JAssetManager> am = context.getAssets();
+	lsp<JInputStream> is = am->open(fileName);
+
+	JFile dir;
+	dir.New(*(lsp<JString>(fileOutPath.getParent()).get()));
+	// 如果目录不存在，则创建。
+	if (false == dir.exists()) {
+		dir.mkdirs();
+	}
+
+	JFileOutputStream os;
+	os.New(outPath);
+
+	JArray<jbyte> buffer;
+	buffer.New(1024);
+
+	// 写文件。
+	jint length;
+	while ((length = is->read(buffer)) > 0) {
+		os.write(buffer, 0, 1024);
+	}
+
+	is->close();
+	os.close();
+}
